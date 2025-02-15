ea2a8e428a374a81dd9988c30a81fb7213d07352
brian tan <brian.tan88@gmail.com>
2025-02-15 18:03:07 -0500

    navigation bar, screens

---
+++ app/src/main/java/com/example/secondnature/ui/navigation/NavigationItem.kt
@@ -0,0 +1,17 @@
+package com.example.secondnature.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class NavigationItem(val route: String, val icon: ImageVector, val title: String) {
+    object Home : NavigationItem("home", Icons.Default.Home, "Home")
+    object Search : NavigationItem("search", Icons.Default.Search, "Search")
+    object Post : NavigationItem("post", Icons.Default.Add, "Post")
+    object History : NavigationItem("history", Icons.Default.AccessTime, "History")
+    object Profile : NavigationItem("profile", Icons.Default.Person, "Profile")
+}
