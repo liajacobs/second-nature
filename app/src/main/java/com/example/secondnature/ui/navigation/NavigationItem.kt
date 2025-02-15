d84a5b818c87ca94c01b80ad87f9dc9ad9cd5c69
brian tan <brian.tan88@gmail.com>
2025-02-15 18:22:34 -0500

    change history icon

--- app/src/main/java/com/example/secondnature/ui/navigation/NavigationItem.kt
+++ app/src/main/java/com/example/secondnature/ui/navigation/NavigationItem.kt
@@ -1,9 +1,9 @@
 package com.example.secondnature.ui.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -12,6 +12,6 @@ sealed class NavigationItem(val route: String, val icon: ImageVector, val title:
     object Home : NavigationItem("home", Icons.Default.Home, "Home")
     object Search : NavigationItem("search", Icons.Default.Search, "Search")
     object Post : NavigationItem("post", Icons.Default.Add, "Post")
-    object History : NavigationItem("history", Icons.Default.AccessTime, "History")
+    object History : NavigationItem("history", Icons.Default.List, "History")
     object Profile : NavigationItem("profile", Icons.Default.Person, "Profile")
 }
